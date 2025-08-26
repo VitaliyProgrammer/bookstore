bf07c8102f212b5b1383f1eaa190e8f06eb9d270
Vitaliy <vitaliyufc@gmail.com>
2025-08-26 15:41:12 +0300

    Initial commit with all project files

---
+++ src/main/java/com/example/basicbookstoreprojectnew/model/repository/BookRepository.java
@@ -0,0 +1,13 @@
+package com.example.basicbookstoreprojectnew.model.repository;
+
+
+import java.util.List;
+import com.example.basicbookstoreprojectnew.model.Book;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository {
+    Book save(Book book);
+
+    List<Book> findAll();
+}
---
+++ src/main/java/com/example/basicbookstoreprojectnew/model/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,31 @@
+package com.example.basicbookstoreprojectnew.model.repository.impl;
+
+import com.example.basicbookstoreprojectnew.model.repository.BookRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.List;
+import com.example.basicbookstoreprojectnew.model.Book;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public Book save(Book book) {
+        if (book.getId() != null) {
+            entityManager.persist(book);
+            return book;
+        } else {
+            return entityManager.merge(book);
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return entityManager.createQuery("SELECT b FROM Book b", Book.class)
+                .getResultList();
+    }
+}
