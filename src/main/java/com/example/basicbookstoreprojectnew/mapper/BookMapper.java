ca713ca27aa7d1d3147fbdb22930a999d9ed4091
Vitaliy <vitaliyufc@gmail.com>
2025-09-01 15:00:31 +0300

    Fixed additional mistakes

--- src/main/java/com/example/basicbookstoreprojectnew/mapper/BookMapper.java
+++ src/main/java/com/example/basicbookstoreprojectnew/mapper/BookMapper.java
@@ -3,7 +3,6 @@ package com.example.basicbookstoreprojectnew.mapper;
 import com.example.basicbookstoreprojectnew.dto.BookDto;
 import com.example.basicbookstoreprojectnew.dto.CreateBookRequestDto;
 import com.example.basicbookstoreprojectnew.model.Book;
-import java.util.List;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
@@ -11,6 +10,4 @@ public interface BookMapper {
     BookDto toDto(Book book);
 
     Book toEntity(CreateBookRequestDto createBookRequestDto);
-
-    List<BookDto> toDoList(List<Book> books);
 }
