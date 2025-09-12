d3ab136200793cd00895fc03451f2064b59f29c1
Vitaliy <vitaliyufc@gmail.com>
2025-09-12 21:12:46 +0300

    Fixed project

---
+++ src/main/java/com/example/basicbookstoreprojectnew/dto/CreateBookRequestDto.java
@@ -0,0 +1,2 @@
+package com.example.basicbookstoreprojectnew.dto;public record CreateBookRequestDto() {
+}
---
+++ src/main/java/com/example/basicbookstoreprojectnew/mapper/BookMapper.java
@@ -0,0 +1,2 @@
+package com.example.basicbookstoreprojectnew.mapper;public class BookMapper {
+}
