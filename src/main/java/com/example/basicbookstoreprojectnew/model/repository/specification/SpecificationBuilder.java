79d0f51415ac591b63f73245013bbf6f4e6a01d7
Vitaliy <vitaliyufc@gmail.com>
2025-09-15 02:56:03 +0300

    Single clean commit with final version

---
+++ src/main/java/com/example/basicbookstoreprojectnew/model/repository/specification/SpecificationBuilder.java
@@ -0,0 +1,8 @@
+package com.example.basicbookstoreprojectnew.model.repository.specification;
+
+import com.example.basicbookstoreprojectnew.dto.BookSearchParametersDto;
+import org.springframework.data.jpa.domain.Specification;
+
+public interface SpecificationBuilder<T> {
+    Specification<T> build(BookSearchParametersDto searchParametersDto);
+}
