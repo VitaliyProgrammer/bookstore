94d6696ac5a4af2f7124f9c415f41a9f33e88e8f
Vitaliy <vitaliyufc@gmail.com>
2025-09-15 03:38:13 +0300

    Single commit without previous history

--- src/main/java/com/example/basicbookstoreprojectnew/model/service/BookService.java
+++ src/main/java/com/example/basicbookstoreprojectnew/model/service/BookService.java
@@ -1,10 +1,20 @@
 package com.example.basicbookstoreprojectnew.model.service;
 
-import com.example.basicbookstoreprojectnew.model.Book;
+import com.example.basicbookstoreprojectnew.dto.BookDto;
+import com.example.basicbookstoreprojectnew.dto.BookSearchParametersDto;
+import com.example.basicbookstoreprojectnew.dto.CreateBookRequestDto;
 import java.util.List;
 
 public interface BookService {
-    Book save(Book book);
+    BookDto save(CreateBookRequestDto bookRequestDto);
 
-    List<Book> findAll();
+    List<BookDto> findAll();
+
+    BookDto findById(Long id);
+
+    BookDto updateBook(Long id, CreateBookRequestDto createBookRequestDto);
+
+    List<BookDto> search(BookSearchParametersDto parametersDto);
+
+    void deleteBook(Long id);
 }
