31116fedbfbef38afe5bd93c203f355f4d870b15
Vitaliy <vitaliyufc@gmail.com>
2025-09-16 14:44:05 +0300

    Add only selected functionality (clean cherry-pick)

--- src/main/java/com/example/basicbookstoreprojectnew/model/repository/BookRepository.java
+++ src/main/java/com/example/basicbookstoreprojectnew/model/repository/BookRepository.java
@@ -3,6 +3,7 @@ package com.example.basicbookstoreprojectnew.model.repository;
 
 import java.util.List;
 import com.example.basicbookstoreprojectnew.model.Book;
+<<<<<<< HEAD
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,4 +11,12 @@ public interface BookRepository {
     Book save(Book book);
 
     List<Book> findAll();
+=======
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+>>>>>>> f9f9180 (Add Specification CriteriaQuery for search-function)
 }
