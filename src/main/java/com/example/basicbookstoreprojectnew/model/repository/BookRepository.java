e1d947c1c01457fc8fab196b126257b5b0328fee
Vitaliy <vitaliyufc@gmail.com>
2025-09-16 16:29:54 +0300

    Fixed commits

--- src/main/java/com/example/basicbookstoreprojectnew/model/repository/BookRepository.java
+++ src/main/java/com/example/basicbookstoreprojectnew/model/repository/BookRepository.java
@@ -1,22 +1,11 @@
 package com.example.basicbookstoreprojectnew.model.repository;
 
-
-import java.util.List;
 import com.example.basicbookstoreprojectnew.model.Book;
-<<<<<<< HEAD
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository {
-    Book save(Book book);
-
-    List<Book> findAll();
-=======
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
->>>>>>> f9f9180 (Add Specification CriteriaQuery for search-function)
+public interface BookRepository extends JpaRepository<Book, Long>,
+        JpaSpecificationExecutor<Book> {
 }
